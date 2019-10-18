f9b398aedecb4c0bc379040ec480a1612603d705
chrishasbeard <chrisholmes16@gmail.com>
2019-10-18 00:24:10 -0400

    final draft, assumming no morning clean up time

--- The Power of Numbers/Program.cs
+++ The Power of Numbers/Program.cs
@@ -6,33 +6,67 @@ namespace The_Power_of_Numbers
     {
 
         static void Main(string[] args)
-        
+
         {
-            string happyFunTime = "  Number| Square|  Cube |";
+            string happyFunTime = "  Number| Square|  Cube |";                
             string userNumber = string.Empty;
-            Console.WriteLine("Learn you squares and cubes!\n\n");
-            Console.Write("Please enter a integer greater than 0: ");
-            userNumber = Console.ReadLine();
-            double num1 = double.Parse(userNumber);
-            Console.WriteLine(happyFunTime);
-            PowerNumberTable(num1);
+            double num1;
+               Console.WriteLine("Learn some squares and cubes!\n\n");
+
+
+            do
+            {
+
+                Console.Write("Please enter a integer greater than 0: ");
+                userNumber = Console.ReadLine();
+                num1 = double.Parse(userNumber);
+                Console.WriteLine(happyFunTime);
+                PowerNumberTable(num1);
+            }
+
+            while (true);
             
         }
-        public static double PowerNumberTable(double number)
+        public static double PowerNumberTable(double num1)
         {
             
             double j;
             double k;
-            for (int i = 1; i <= number; i++)
+            for (int i = 1; i <= num1; i++)
             {
-               
-                i = i;
                 j = i * i;
                 k = i * i * i;
                 
                 Console.WriteLine($"{i}\t|{j}\t|{k}\t|");
+                
             }
-            return number;
+            Console.WriteLine("Do you want to keep learning?: (y/n) ");
+            KeepGoing();
+            return num1;
+        }
+        public static double CheckingNonZero(double num1)
+        {
+            while (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("That was invalid. Enter a number greater than Zero, Silly.");
+            }
+            return num1;
+        }
+        public static bool KeepGoing ()
+        {
+            
+            string yesNo = Console.ReadLine().ToLower();
+            switch (yesNo)
+            {
+                case "y":
+                    yesNo = "y";
+                    return true;
+                case "n":
+                    yesNo = "n";
+                    return false;
+                default:
+                    return KeepGoing();
+            }
         }
         //make a method to ensure a number was entered.
         //make a method to continue use a switch.
---
+++ sidebiscuit/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace The_Power_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string happyFunTime = "  Number| Square|  Cube |";
+            double num1;
+
+            Console.WriteLine("Learn you squares and cubes!\n\n");
+            Console.Write("Please enter a integer greater than 0: ");
+            string userNumber = Console.ReadLine();
+            num1 = Convert.ToDouble(userNumber);
+            PowerNumberTable(num1);
+            CheckingNonZero(num1);
+            Console.WriteLine(happyFunTime);
+            Console.WriteLine("Do you want to enter another number?(y/n): ");
+            Console.WriteLine("Do you want to enter another number?(y/n): ");
+            string yesNo = Console.ReadLine().ToLower();
+
+        }
+
+    }
+    public static double PowerNumberTable(double number)
+    {
+        string userNumber;
+        double j;
+        double k;
+        for (int i = 1; i <= number; i++)
+        {
+            j = i * i;
+            k = i * i * i;
+            Console.WriteLine($"{i}\t|{j}\t|{k}\t|");
+        }
+        return number;
+
+    }
+    public static double CheckingNonZero(double num1)
+    {
+
+        while (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("That was invalid. Enter a number greater than Zero, Silly.");
+        }
+        return num1;
+    }
+    public static bool KeepGoing()
+    {
+        Console.WriteLine("Do you want to enter another number?(y/n): ");
+        string yesNo = Console.ReadLine().ToLower();
+        switch (yesNo)
+        {
+            case "y":
+                yesNo = "y";
+                return true;
+            case "n":
+                yesNo = "n";
+                return false;
+            default:
+                return KeepGoing();
+        }
+    }
+    
+
+        //make a method to ensure a number was entered.
+        //make a method to continue use a switch.
+    
